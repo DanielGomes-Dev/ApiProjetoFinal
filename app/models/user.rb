0738053bb296e258094ebf1a5093263ed3604c54
Daniel Gomes <danielgds@id.uf.br>
2021-01-26 22:06:47 -0300

    Teste Para Push

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,12 @@ class User < ApplicationRecord
     has_one :address
     has_secure_password
 
-<<<<<<< HEAD
     #name
     validates :name, presence: true
 
     #email
     validates :email, presence: true
     validates :email, uniqueness: true
-=======
-    #presenca obrigatoria!
-    validates :name, :email, :cpf, :rg, :role, :password, :nationality, :birthdate, :state, presence: true
-    #Cpf no formato certo!
-    {with: /\A(d{3}+.){2}+d{3}+-+d{2}\Z/, message: "O CPF deve estar no formato XXX.XXX.XXX-XX"}
-    #Email tem que ser da idUFF!
->>>>>>> 122bc5fa8f6d818fc346ac72acf00ca1ad7c62f0
     validates :email, format: {with: /\b[A-Z0-9._%a-z\-]+@id\.uff\.br\z/, message: "O email deve ser da idUFF"}
 
     #CPF
