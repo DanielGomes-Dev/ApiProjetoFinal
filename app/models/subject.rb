efced91f799ddd7032a73567f8ba707e497b350f
Daniel Gomes <danielgds@id.uf.br>
2021-01-27 17:42:09 -0300

    merge

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,8 +2,5 @@ class Subject < ApplicationRecord
   has_many :can_lectures
   has_many :professors, through: :can_lectures
   has_many :classrooms
-
-  has_many :appointments
-  has_many :school_years, through: :appointments
-
+  
 end
