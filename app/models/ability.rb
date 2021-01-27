f1a4ebc4a78fe424fa433d0b5fc374a0336c68a7
Gabriel Taveira <gabriel@sereno.com.br>
2021-01-27 18:08:08 -0300

    deploy 3.0

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
-
-    #caso o usuario seja o diretor
-    if Manager.present?
-      #r cadastra o curso, com as seguintes credenciais: nome, area de conhecimento, código, campus sede.
-      :create, Course
     
     end
   end
