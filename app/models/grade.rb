2069e58b211e363949f92a03e83b6781c2faa20f
Gabriel Taveira <gabriel@sereno.com.br>
2021-01-27 12:25:37 -0300

    Tentativa de consertar o erro na grade:14

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -9,7 +9,7 @@ class Grade < ApplicationRecord
   belongs_to :professor
   
   has_many: get_grades
-  has_many:students, through: get_grade
+  has_many: students, through: get_grade
 
   has_many: launch_grades
   has_many: professors, through:launch_grade
