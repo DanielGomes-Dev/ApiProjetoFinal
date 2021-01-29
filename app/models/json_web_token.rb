234a524f97b7fa33dc831715dc55e675b57b1e20
Daniel Gomes <danielgds@id.uf.br>
2021-01-28 23:04:14 -0300

    tentando resolver o retorno2

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,13 +1,14 @@
 class JsonWebToken
-    Secret = Rails.application.secrets.secret_key_base
+    # Secret = Rails.application.secrets.secret_key_base
+    secret = "Rails.application.secrets.secret_key_base"
 
     def self.encode(payload)
-        return JWT.encode(payload, Secret)
+        return JWT.encode(payload, secret)
     end
 
     def self.decode (token)
         begin
-            return JWT.decode(token,Secret)
+            return JWT.decode(token, secret)
         rescue => exception
             puts "Não conseguiu pegar Decodificar o token"
             return nil
