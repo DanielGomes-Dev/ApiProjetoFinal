37c52f09e39475512965dd9a6a30de5e0a989f96
Gabriel Taveira <gabriel@sereno.com.br>
2021-01-30 15:51:04 -0300

    cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,32 +3,32 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  end
+  =begin 
+  #def initialize(user)
+   # if user.manager.present?
+    #  can :manage, Course, Coordinator
+     # can :manage, School_year
+      #can :manage, Department
+  #end
+   # if user.coordinator.present?
+      #can :manage, user.coordinator.Courses
+      #can :manage, Student
+      #Durante o planejamento
+      #can :create, Subject, Classroom
+      #can :manage, Professor, Course
+      #Durante o período de inscrições
+     # can :read, Subject
+
+    #end
+    #if User.present?
+     # can :read, course_id
+    #end
+    #if user.professor.present?
+      #Enquanto o período estiver aberto
+     # can :manage, Grade
+    #end
+    #if user.student.present?
+    #end
+  
+
 end
--- db/migrate/20210130180840_create_blas.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlas < ActiveRecord::Migration[6.1]
-  def change
-    create_table :blas do |t|
-
-      t.timestamps
-    end
-  end
-end
